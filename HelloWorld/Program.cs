ef848d15dd76a22e709599d6f551be33e2f87a92
Haeun Kwon <s2118@e-mirim.hs.kr>
2023-04-03 12:05:32 +0900

    #1 한글로 안녕 세상아로 출력 내용 수정

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; 
+
 
 namespace HelloWorld
 {
@@ -10,7 +7,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("안녕 세상아");
         }
     }
 }
